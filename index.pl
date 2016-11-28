45d8f31c86d8833fd93410747b4d56631604ab08
Kai <titshacking@users.noreply.github.com>
2016-11-28 15:26:49 +0530

    Update index.pl

--- index.pl
+++ index.pl
@@ -8,12 +8,13 @@ use CGI;
 use CGI::Carp 'fatalsToBrowser';
 
 my $resHTML = CGI->new;
+my $intDefaultSize = 300;
 
-if ($resHTML->param()) {
+if ($resHTML->param('items')) {
     my $strItems = $resHTML->param('items');
     my $intSize = $resHTML->param('size');
     my $resAvatar = Avatar->new("http://mobcdn.clubpenguin.com/game/items/images/paper/image/");
-    $resAvatar->constructAvatar($strItems, $intSize);
+    $resAvatar->constructAvatar($strItems, ($intSize ? $intSize : $intDefaultSize));
 }
 
 1;
